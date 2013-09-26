6c916d03bf8a1c79201f12f2eccdb3a03aade4b6
Aymeric Spiga <aymeric.spiga@upmc.fr>
2013-09-26 11:12:23 +0000

    planetoplot. added an example to use ppplot to plot a sounding very easily.
    git-svn-id: http://svn.lmd.jussieu.fr/Planeto/trunk/UTIL/PYTHON/planetoplot_v2@1051 ca07c32c-8b1f-4cef-a7c7-6a85da90e321

---
+++ examples/scripts_ppplot_only/plot_sounding.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+import ppplot
+import numpy as np
+
+# load data
+# http://docs.scipy.org/doc/numpy/reference/generated/numpy.loadtxt.html
+p,z,t,dew,hum,mix,wdir,wknot,thta,thte,thtv \
+    = np.loadtxt("sounding.txt",skiprows=8,unpack=True)
+
+# make a simple plot
+sdg = ppplot.plot1d()
+sdg.f = z
+sdg.x = t
+sdg.makeshow()
+
+# Tweaking a little bit the plot
+sdg.linestyle = '-'
+sdg.marker = '.'
+sdg.color = 'r'
+sdg.ycoeff = 1.e-3
+sdg.title = "A random terrestrial sounding"
+sdg.legend = "Fort Smith 00Z 26 Sep 2013"
+sdg.xlabel = "Temperature ($^{\circ}$C)"
+sdg.ylabel = "Altitude (km)"
+sdg.makeshow()
+
+
