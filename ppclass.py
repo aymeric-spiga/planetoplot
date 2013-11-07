407fd12df4b890985eda0e5f5a55d83fdaa79c8d
Aymeric Spiga <aymeric.spiga@upmc.fr>
2013-11-07 09:02:02 +0000

    PLANETOPLOT. bug fix on a bad condition. that made some lat/lon axis inoperate for wrong reasons.
    git-svn-id: http://svn.lmd.jussieu.fr/Planeto/trunk/UTIL/PYTHON/planetoplot_v2@1090 ca07c32c-8b1f-4cef-a7c7-6a85da90e321

--- ppclass.py
+++ ppclass.py
@@ -1385,7 +1385,7 @@ class onerequest():
                self.field_y = self.field_y[0,:,:]
           # if xy axis are apparently undefined, set 2D grid points axis.
           if "grid points" not in self.name_x:
-            if self.field_x.all() == self.field_x[0,0] \
+            if np.all(self.field_x == self.field_x[0,0]) \
              or self.field_x.min() == self.field_x.max() \
              or self.field_y.min() == self.field_y.max():
                if self.verbose: print "!! WARNING !! xy axis look undefined. creating non-dummy ones."
