6ef188c890537c83a85231cb50761f87db9e6b60
Aymeric Spiga <aymeric.spiga@upmc.fr>
2014-02-04 22:57:26 +0000

    planetoplot. added an example script for movie. added an example script for masking.
    git-svn-id: http://svn.lmd.jussieu.fr/Planeto/trunk/UTIL/PYTHON/planetoplot_v2@1176 ca07c32c-8b1f-4cef-a7c7-6a85da90e321

---
+++ examples/movie/movie.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+from ppclass import pp
+
+
+opac = pp()
+opac.file = "../tau.nc"
+opac.var = "TAU"
+opac.contour = "HGT"
+#opac.changetime = "mars_meso_lt"
+opac.vmin = 0.
+opac.vmax = 4.
+opac.back = "vishires"
+opac.title = ""
+opac.proj = "ortho"
+opac.out = "png"
+opac.res = 75
+
+count = 0
+for ttt in range(0,135,3):
+  opac.filename = "taustorm"+"%03d"%(count)
+  opac.includedate = False
+  opac.t = ttt
+  opac.getplot()
+  count = count+1
