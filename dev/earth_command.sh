bfeb3b88f6001128cb8aa76248f14180ff62c383
aymeric-spiga <aymeric.spiga@upmc.fr>
2017-12-10 17:57:12 +0100

    added a dev folder with stuff for dev

---
+++ dev/earth_command.sh
@@ -0,0 +1,4 @@
+pp.py NPv4.12_SE_1982_1989_1M_histmth.nc -v tops -t 2 -B contrast -P
+ortho --trans 0.7 --vmin 0 --vmax 400
+pp.py NPv4.12_SE_1982_1989_1M_histmth.nc -v topl -t 2 -B contrast -P
+ortho --trans 0.7 --vmin 100 --vmax 300 
---
+++ dev/test_earth_calendar.sh
@@ -0,0 +1,1 @@
+./pp.py /home/aymeric/TMPDIR/histday.nc -v precip -x 0 -y 0 -s earth_calendar
